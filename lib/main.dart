dad5bcd5771d9d2bd0d516d04ed9a3f0292f141a
alhasann351 <alhasann352@gmail.com>
2024-02-11 12:05:34 +0600

    google font added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,32 +32,64 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Rich Text'),
       ),
-      body: RichText(
-        text: const TextSpan(
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 25,
-          ),
-          children: [
-            TextSpan(text: 'Flutter '),
-            TextSpan(
-              text: 'Apps ',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          RichText(
+            text: const TextSpan(
               style: TextStyle(
-                fontSize: 27,
-                color: Colors.blueAccent,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            TextSpan(
-              text: 'Development ',
-              style: TextStyle(
+                color: Colors.black,
                 fontSize: 25,
-                color: Colors.redAccent,
-                fontStyle: FontStyle.italic,
               ),
+              children: [
+                TextSpan(text: 'Flutter '),
+                TextSpan(
+                  text: 'Apps ',
+                  style: TextStyle(
+                    fontSize: 27,
+                    color: Colors.blueAccent,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: 'Development ',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.redAccent,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          RichText(
+            text: TextSpan(
+              style: const TextStyle(
+                fontSize: 30,
+                color: Colors.amber,
+              ),
+              children: [
+                const TextSpan(text: 'Hello '),
+                TextSpan(
+                  text: 'Flutter ',
+                  style: GoogleFonts.amaranth(
+                    fontSize: 32,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.green,
+                  ),
+                ),
+                TextSpan(
+                  text: 'Developer',
+                  style: GoogleFonts.pacifico(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.pink,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
